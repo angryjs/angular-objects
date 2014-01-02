ac3307731a6d2e62d9b58db1a810fbd95ec153a9
JSifalda <sifalda.jiri@gmail.com>
2014-01-02 22:12:51 +0100

    Add source

---
+++ src/angular-objects.js
@@ -0,0 +1,18 @@
+(function(angular, undefined) {
+  'use strict';
+
+  angular.module('angryjs.objects', [])
+
+    .factory('Objects', function () {
+      var Objects = function () {};
+      Objects.prototype.merge = function (obj1, obj2) {
+        var obj3 = {};
+        for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+        for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+        return obj3;
+      };
+
+      return new Objects();
+    });
+
+})(window.angular);
